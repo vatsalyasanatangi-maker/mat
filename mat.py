919910d445856570824c7b1ef854b29e6442deb3
vatsalyasanatangi <vatsalyasanatangi@gmail.com>
2026-01-14 10:24:58 +0530

    add mat program

--- mat.py
+++ mat.py
@@ -2,3 +2,7 @@ a=float(input("Enter first number:"))
 b=float(input("Enter second number:"))
 print(f"Addition:{a+b}")
 print(f"Subtaction:{a-b}")
+if b != 0:
+  print(f"Division:{a/b}")
+else:
+  print("Division by zero is not allowed.")
